59278ac12237e29dbb6c44756cc354fc1a6620a5
VarunBhola-Netsol <varun.bhola@netsolutionsindia.com>
2016-06-01 14:38:04 +0530

    Update models.py

--- hello/apps/hellocount/models.py
+++ hello/apps/hellocount/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Visit(models.Model):
     visit_stamp = models.DateTimeField(auto_now_add=True)
     user_agent = models.CharField(max_length=255)
+    create_date = models.DateTimeField(auto_now_add=True, null=True)
 
